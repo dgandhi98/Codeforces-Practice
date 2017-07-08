967d32118669e777f38e98d7b32d3698d2d3f8ed
dgandhi98 <divyanshugandhi@yahoo.com>
2017-07-08 17:46:03 -0400

    7/8/17

--- A007.cpp
+++ A007.cpp
@@ -2,6 +2,34 @@
 using namespace std;
 
 int main() {
-  cout << "Hello World" << endl;
+  int numRows;
+  int numCols;
+  string rows[8];
+  for(int i = 0; i < 8;i++)
+    getline(cin, rows[i]);
+
+  //check rows
+  for(int i = 0; i < 8; i++) {
+    if(rows[i]=="BBBBBBBB") {
+      numRows++;
+    }
+  }
+
+  //check cols
+  if(numRows!=8) {
+    for(int i = 0; i < 8; i++) {
+      if(rows[0][i]=='B') {
+        for(int j = 0; j < 8; j++) {
+          if(rows[j][i]!='B') {
+            break;
+          }
+          else if(j==7) {
+            numCols++;
+          }
+        }
+      }
+    }
+  }
+  cout << numRows + numCols <<endl;
   return 0;
 }
